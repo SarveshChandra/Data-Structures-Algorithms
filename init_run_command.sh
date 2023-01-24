5cb2eafe1f284661bf83d874fd1e94a61d277413
SarveshChandra <sarvesh071191@gmail.com>
2023-01-24 16:18:44 +0530

    save

--- init_run_command.sh
+++ init_run_command.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-add-apt-repository ppa:deadsnakes/ppa
-apt update
-apt -y install g++
-apt install software-properties-common
-apt install python3.8
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt -y install g++
+sudo apt install software-properties-common
+sudo apt install python3
 
-cat define_run.sh >> /root/.bashrc
+cat define_run.sh >> ~/.bashrc
 
 exec bash
